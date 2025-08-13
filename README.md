09c4790dfeda384c7a58bfc9ec519a8ff6e4b0ab
Salman Malik <salman.the.malik@gmail.com>
2025-08-13 17:22:11 -0400

    adding some changes

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
-# spring-boot-basics
+# spring-boot-gzip
 
-This repository tracks the progress of our KT sessions, as we introduce more and more spring-framework and spring-boot features. The main areas of focus in the order they were introduced are:
+A sample spring-boot app that demonstrates usage of gzip when serving/consuming HTTP content.
 
-- MVC controllers
-- Inversion of Control (IOC)
-- Dependency Injection
-- Externalized Configuration
-- Spring Profiles
-- JDBC repositories
-- Making REST calls
+The following endpoints demo serving of gzip'ed content (if the content is larger than 1KB):
+
+- `/tiny`
+- `/small`
+- `/large`
+
+The `/tiny` content will not be compressed, the other two will be if `Accept-Encoding: gzip` HTTP header is part of the request.   
+
+The `/client` endpoint acts as the HTTP client of its own 3 endpoints and reports information about the response. Check the application log for wire-level information about these requests as they are processed using `org.apache.hc.client5` libraries through the `org.springframework.web.client.RestClient` abstraction.
+
 
 ## Building
 
@@ -19,58 +22,5 @@ This repository tracks the progress of our KT sessions, as we introduce more and
 ## Running
 
 ```
-java -jar target/demo*.jar
+java -jar target/spring-boot-gzip-1.0.jar
 ```
-
-If running with a specific profile, for example `db`, then run:
-
-```
-java -jar target/demo*.jar --spring.profiles.active=db
-```
-
-To access the `heroes` endpoint using `curl` and `jq`, run:
-
-```
-curl -s localhost:8080/heroes | jq .
-```
-
-To access the `heroes` endpoint using `http` command (from [httpie](https://httpie.io/cli) project), run:
-```
-http localhost:8080/heroes
-```
-
-## Postgres SQL
-
-If running with Postgres DB, you will need to either start a DB instance in docker or modify the configuration values specified in [src/main/resources/application-db.yml](src/main/resources/application-db.yml). To start a postgres instance running in docker, run:
-
-```
-docker run -d --rm \
-    --name postgres \
-    -e POSTGRES_PASSWORD=secret \
-    -p 5432:5432 \
-    postgres:15
-```
-
-To run `psql` commands against the DB (like `create table ...`, `delete from ...`, or any other SQL), run:
-
-```
-docker exec -it postgres \
-    psql -U postgres -d postgres
-```
-
-To terminate the container and all the data in it, run:
-
-```
-docker rm -f postgres
-```
-
-## Running Local *httpbin*
-
-```
-docker run -d --rm \
-    --name httpbin \
-    -p 8585:80 \
-    kennethreitz/httpbin
-```
-# spring-boot-gzip
-# spring-boot-gzip
--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.io.Resource;
@@ -14,9 +13,7 @@ import org.springframework.web.client.RestClient;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class DemoController {
@@ -35,39 +32,44 @@ public class DemoController {
 
     @GetMapping("/small")
     public ResponseEntity<String> getSmallContent() throws IOException {
-        return getResponse(smallContent);
+        return generateResponse(smallContent);
     }
 
     @GetMapping("/large")
     public ResponseEntity<String> getLargeContent() throws IOException {
-        return getResponse(largeContent);
+        return generateResponse(largeContent);
     }
 
     @GetMapping("/tiny")
     public ResponseEntity<String> getTinyContent() throws IOException {
-        return getResponse(tinyContent);
+        return generateResponse(tinyContent);
 
     }
 
+    private ResponseEntity<String> generateResponse(Resource resource) throws IOException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        return new ResponseEntity<>(resource.getContentAsString(Charset.defaultCharset()), headers, HttpStatus.OK);
+    }
+
     @GetMapping("/client")
-    public Map<String, ClientResponse> doClientStuff() throws IOException {
+    public List<ClientResponse> doClientStuff(@Value("${server.port:8080}") int port) throws IOException {
 
         ObjectMapper mapper = new ObjectMapper();
-        mapper.enable(SerializationFeature.INDENT_OUTPUT);
-
-        Map<String, ClientResponse> responses = new HashMap<>();
         RestClient client = RestClient.builder().build();
-        for (String name : List.of("tiny", "small", "large")) {
-            ResponseEntity<List<DataElement>> entity = client.get()
-                    .uri("http://localhost:8080/" + name)
-                    .retrieve()
-                    .toEntity(new ParameterizedTypeReference<List<DataElement>>() {});
+        List<ClientResponse> responses = new ArrayList<>();
 
-            responses.put(name,
-                    new ClientResponse(name,
-                            mapper.writeValueAsString(entity.getBody()).length(),
-                            entity.getBody().size()));
+        for (String name : List.of("tiny", "small", "large")) {
+            String uri = String.format("http://localhost:%d/%s", port, name);
+            List<DataElement> data = client.get()
+                    .uri(uri)
+                    .retrieve()
+                    .body(new ParameterizedTypeReference<>() {});
+            responses.add(new ClientResponse(name,
+                    mapper.writeValueAsString(data).length(),
+                    data.size()));
         }
+
         return responses;
     }
 
@@ -79,13 +81,7 @@ public class DemoController {
 
     record ClientResponse(
         String name,
-        long contentLength,
-        int elements
+        long dataSize,
+        int dataElements
     ) {}
-
-    private ResponseEntity<String> getResponse(Resource resource) throws IOException {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/json");
-        return new ResponseEntity<>(resource.getContentAsString(Charset.defaultCharset()), headers, HttpStatus.OK);
-    }
 }
